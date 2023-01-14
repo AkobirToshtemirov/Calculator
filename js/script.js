5a0ae2e7519d45da7147213053d7e0c1ab1b0aec
Akobir5 <toshtemirovakobir8@gmail.com>
2023-01-14 14:55:48 +0500

    add cal

--- js/script.js
+++ js/script.js
@@ -1,70 +1,22 @@
 let showCalc = document.querySelector('#show__num');
-let btns = document.querySelectorAll('.btn__num');
-let clear = document.getElementById('clear');
-let percent = document.getElementById('percent');
-let division = document.getElementById('division');
-let multiplication = document.getElementById('multiplication');
-let minus = document.getElementById('minus');
-let plus = document.getElementById('plus');
-let equal = document.getElementById('equal');
-let allCalc = document.querySelectorAll('.allCalc');
+let btns = document.querySelectorAll('.btn');
 
-let firstValue = [];
-let secondValue = [];
-
-let isTrue;
-let calcMain;
-
-allCalc.forEach(item => {
+btns.forEach(item => {
   item.addEventListener('click', () => {
-    change(item);
-    calcMain = item.textContent;
-    console.log(calcMain);
-    let isTrueNext;
-    isTrueNext = false;
+    let id = item.id;
+
+    if(id == 'clear') {
+      showCalc.innerText = '';
+    } else if (id == 'backspace') {
+      let string = showCalc.innerText.toString();
+      showCalc.innerText = string.substr(0, string.length - 1);
+    } else if(showCalc.innerText != '' && id =='equal') {
+      showCalc.innerText = eval(showCalc.innerText)
+    } else if(showCalc.innerText == '' && id == 'equal') {
+      showCalc.innerText = 'Empty!';
+      setTimeout(() => (showCalc.innerText = '', 2000))
+    } else {
+      showCalc.innerText += id;
+    }
   })
 })
-
-console.log(isTrue);
-// clear.addEventListener('click', () => {
-//   clearValue();
-// })
-
-do {
-  btns.forEach((item, index) => {
-    item.addEventListener('click', () => {
-      firstValue.push(item.textContent)
-      showCalc.innerHTML = firstValue.join('');
-
-      for(let i = 0; i < btns.length; i++) {
-        btns[i].classList.add('btn__next__one')
-      }
-    })
-  })
-
-} while (isTrue)
-
-let btnsNext = document.querySelectorAll('.btn__next__one')
-
-  btnsNext.forEach((item, index) => {
-    item.addEventListener('click', () => {
-      secondValue.push(item.textContent)
-      showCalc.innerHTML = secondValue.join('');
-    })
-  })
-
-
-
-function change(showNewValue) {
-  isTrue = false;
-  showCalc.innerHTML = showNewValue.textContent;
-}
-
-// switch (calcMain) {
-//   case '+':
-
-// }
-
-console.log(firstValue)
-console.log("=====");
-console.log(secondValue);
